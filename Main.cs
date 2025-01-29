cde201099c3a778254966eef41055524c7ed0dc9
Jerry <2963025245@qq.com>
2025-01-29 13:13:04 +0800

    添加窗口

--- Main.cs
+++ Main.cs
@@ -4,29 +4,38 @@ using System;
 
 public partial class Main : Node2D
 {
-	private double time = 0;
-	// Called when the node enters the scene tree for the first time.
-	private Label la;
-	Dictionary dates = Time.GetDatetimeDictFromSystem();
-	public override void _Ready()
-	{
-		 la = GetNode<Label>("/root/main/Label");
+    private Label la;
+    private Timer times;
 
-		// Timer times = GetNode<Timer>("Timer") ;
-		// times.Start();
+    public override void _Ready()
+    {
+        la = GetNode<Label>("/root/main/Label");
+		times = new Timer(); 
+		this.AddChild(times);
+        times.WaitTime = 1.0f; // 设置定时器每秒触发一次
+        times.Connect("timeout", new Callable(this, "hhh"));
+        times.Start(); // 启动定时器
+    }
 
-		// var currentTime = Time.GetDateDictFromSystem();
-        // GD.Print("当前时间: " + currentTime);
-	}
+    public override void _Process(double delta)
+    {
+        // 这里可以添加其他逻辑，如果需要的话
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		Dictionary dates = Time.GetDatetimeDictFromSystem();
-		String s1 = (String)dates["hour"];
-		String s2 = (String)dates["minute"];
-		String s3 = (String)dates["second"];
-		String ss = s1+'/'+s2+'/'+s3;
-		la.Text= ss;
-	}
+    private void hhh()
+    {
+        // 获取当前时间
+        Dictionary dates = Time.GetDatetimeDictFromSystem();
+        String s1 = (String)dates["hour"]; // 使用安全转换
+        String s2 = (String)dates["minute"];
+        String s3 = (String)dates["second"];
+        String ss = s1 + ':' + s2 + ':' + s3;
+        la.Text = ss; // 更新Label文本
+    }
+    private void clickButton()
+    {
+        PackedScene newScene = GD.Load<PackedScene>("res://Mission.tscn");
+        Node nodes = newScene.Instantiate();
+        this.GetTree().CurrentScene.AddChild(nodes);
+    }
 }
